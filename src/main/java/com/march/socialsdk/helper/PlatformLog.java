5e99c0b7847bf91a1cb57d0ee95b3fd7fd48a54a
luyuan <1101873740@qq.com>
2017-12-05 10:57:15 +0800

    修改log类

--- src/main/java/com/march/socialsdk/helper/PlatformLog.java
+++ src/main/java/com/march/socialsdk/helper/PlatformLog.java
@@ -2,6 +2,8 @@ package com.march.socialsdk.helper;
 
 import android.util.Log;
 
+import com.march.socialsdk.BuildConfig;
+
 /**
  * CreateAt : 2016/12/22
  * Describe : log
@@ -11,7 +13,8 @@ import android.util.Log;
 
 public class PlatformLog {
 
-    public static boolean DEBUG = true;
+    public static final String TAG = "social-sdk";
+    public static boolean DEBUG = BuildConfig.DEBUG;
 
     private static String getMsg(Object msg) {
         return msg == null ? "null" : msg.toString();
@@ -19,7 +22,7 @@ public class PlatformLog {
 
     public static void e(String tag, Object msg) {
         if (DEBUG)
-            Log.e(tag + "|platform", getMsg(msg));
+            Log.e(tag + "|" + TAG, getMsg(msg));
     }
 
     public static void e(String tag, Object... msg) {
@@ -28,12 +31,12 @@ public class PlatformLog {
             for (Object o : msg) {
                 sb.append(" ").append(getMsg(o)).append(" ");
             }
-            Log.e(tag + "|platform", sb.toString());
+            Log.e(tag + "|" + TAG, sb.toString());
         }
     }
 
     public static void e(Object msg) {
         if (DEBUG)
-            Log.e("platform", getMsg(msg));
+            Log.e(TAG, getMsg(msg));
     }
 }
